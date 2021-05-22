dd42683d0be9298ce0cfdba6186a2b05bbffa942
Marcin Makarewicz <marcin@makarewicz.eu>
2021-05-23 01:00:39 +0200

    feat: add cannon physics

--- packages/web/src/Game1.tsx
+++ packages/web/src/Game1.tsx
@@ -1,23 +1,41 @@
 import * as THREE from "three";
 import React, { Suspense, useEffect, useRef, useState } from "react";
 import { Canvas, useFrame, useThree } from "react-three-fiber";
+import { Physics, usePlane, useBox } from "@react-three/cannon";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { config, useSpring } from "@react-spring/core";
-// import { a } from "@react-spring/three";
 import useKeyPress from "./hooks/useKeyPress";
 import Island from "./components/Island.tsx";
 import styles from "./App.module.scss";
 
 const calcPos = ({ position: { x, y, z } }, h = 1) => [x, y + h, z];
 
-const calcCam = ({ position: { x, y, z }, rotation }, d = 3, h = 3) => [
-  x + Math.sin(rotation.y) * d,
-  y + h,
-  z + Math.cos(rotation.y) * d,
-];
+function Plane(props) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
+
+  return (
+    <mesh ref={ref} receiveShadow>
+      <planeBufferGeometry attach="geometry" args={[15, 15]} />
+      <shadowMaterial attach="material" color="#171717" />
+    </mesh>
+  );
+}
 
 function Boy({ parameter, ...props }) {
-  const ref = useRef();
+  const [rotation, setRotation] = useState(0);
+  const [ref, api] = useBox(() => ({
+    mass: 1,
+    position: [0, 3, 0],
+    isKinematic: true,
+  }));
+
+  const calcCam = ({ position: { x, y, z } }, d = 3, h = 3) => [
+    x + Math.sin(rotation) * d,
+    y + h,
+    z + Math.cos(rotation) * d,
+  ];
+
+  Object.assign(window, { ref, api });
   const { gl, camera } = useThree();
   const { scene } = useGLTF("boy.gltf");
   const [{ pos, cam }, set] = useSpring(() => ({
@@ -31,11 +49,14 @@ function Boy({ parameter, ...props }) {
     ArrowDown: useKeyPress("ArrowDown"),
     ArrowLeft: useKeyPress("ArrowLeft"),
     ArrowRight: useKeyPress("ArrowRight"),
+    Space: useKeyPress(" "),
   };
   useEffect(() => {
     if (pressed.ArrowUp) {
-      ref.current.position.x -= Math.sin(ref.current.rotation.y);
-      ref.current.position.z -= Math.cos(ref.current.rotation.y);
+      const {
+        position: { x, y, z },
+      } = ref.current;
+      api.position.set(x - Math.sin(rotation), y, z - Math.cos(rotation));
     }
     set({
       pos: calcPos(ref.current),
@@ -44,8 +65,10 @@ function Boy({ parameter, ...props }) {
   }, [pressed.ArrowUp]);
   useEffect(() => {
     if (pressed.ArrowDown) {
-      ref.current.position.x += Math.sin(ref.current.rotation.y);
-      ref.current.position.z += Math.cos(ref.current.rotation.y);
+      const {
+        position: { x, y, z },
+      } = ref.current;
+      api.position.set(x + Math.sin(rotation), y, z + Math.cos(rotation));
     }
     set({
       pos: calcPos(ref.current),
@@ -54,27 +77,35 @@ function Boy({ parameter, ...props }) {
   }, [pressed.ArrowDown]);
   useEffect(() => {
     if (pressed.ArrowLeft) {
-      ref.current.rotation.y += Math.PI / 4;
+      setRotation((rotation) =>
+        ((y) => {
+          api.rotation.set(0, y, 0);
+          return y;
+        })(rotation + Math.PI / 4)
+      );
     }
-    set({
-      pos: calcPos(ref.current),
-      cam: calcCam(ref.current),
-    });
   }, [pressed.ArrowLeft]);
   useEffect(() => {
     if (pressed.ArrowRight) {
-      ref.current.rotation.y -= Math.PI / 4;
+      setRotation((rotation) =>
+        ((y) => {
+          api.rotation.set(0, y, 0);
+          return y;
+        })(rotation - Math.PI / 4)
+      );
     }
-    set({
-      pos: calcPos(ref.current),
-      cam: calcCam(ref.current),
-    });
   }, [pressed.ArrowRight]);
+  useEffect(() => {
+    if (pressed.Space) {
+      api.velocity.set(0, 4, 0);
+    }
+  }, [pressed.Space]);
 
-  useFrame((state) => {
-    // const t = state.clock.getElapsedTime();
-    camera.lookAt(new THREE.Vector3(...pos.get()));
-    (([x, y, z]) => Object.assign(camera.position, { x, y, z }))(cam.get());
+  useFrame(({ clock }) => {
+    const t = clock.getElapsedTime();
+    const [x, y, z] = pos.get();
+    camera.lookAt(new THREE.Vector3(x, 2, z));
+    camera.position.set(...cam.get());
   });
 
   return <primitive ref={ref} object={scene} {...props} />;
@@ -121,10 +152,15 @@ export default function Demo() {
         camera={{ position: [-1, 2, 5], fov: 50 }}
       >
         <Suspense fallback="loading...">
-          <Island scale={2} />
-          <Boy position={[0, 0.6, 0]} scale={[0.1, 0.1, 0.1]} />
+          <Island scale={1} />
+          <Physics>
+            <Plane />
+            <Boy position={[0, 1, 0]} scale={[0.1, 0.1, 0.1]} />
+          </Physics>
         </Suspense>
         <OrbitControls />
+        <color attach="background" args={["lightblue"]} />
+        <hemisphereLight intensity={0.35} />
         <spotLight
           position={[10, 10, 10]}
           angle={0.3}
