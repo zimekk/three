b45dc00574940d91fd053229a7a0979d91c709eb
Marcin Makarewicz <marcin@makarewicz.eu>
2021-06-21 20:06:40 +0200

    chore: update deps

--- packages/web/src/Minecraft/Player.tsx
+++ packages/web/src/Minecraft/Player.tsx
@@ -16,6 +16,8 @@ const moveFieldByKey = (key) => keys[key];
 const direction = new THREE.Vector3();
 const frontVector = new THREE.Vector3();
 const sideVector = new THREE.Vector3();
+// const rotation = new THREE.Vector3()
+const speed = new THREE.Vector3();
 
 const usePlayerControls = () => {
   const [movement, setMovement] = useState({
@@ -50,12 +52,9 @@ export const Player = (props) => {
   const { forward, backward, left, right, jump } = usePlayerControls();
   const { camera } = useThree();
   const velocity = useRef([0, 0, 0]);
-  useEffect(
-    () => void api.velocity.subscribe((v) => (velocity.current = v)),
-    []
-  );
-  useFrame(() => {
-    camera.position.copy(ref.current.position);
+  useEffect(() => api.velocity.subscribe((v) => (velocity.current = v)), []);
+  useFrame((state) => {
+    ref.current.getWorldPosition(camera.position);
     frontVector.set(0, 0, Number(backward) - Number(forward));
     sideVector.set(Number(left) - Number(right), 0, 0);
     direction
@@ -63,9 +62,18 @@ export const Player = (props) => {
       .normalize()
       .multiplyScalar(SPEED)
       .applyEuler(camera.rotation);
+    speed.fromArray(velocity.current);
+    // axe.current.children[0].rotation.x = THREE.MathUtils.lerp(
+    //   axe.current.children[0].rotation.x,
+    //   Math.sin((speed.length() > 1) * state.clock.elapsedTime * 10) / 6,
+    //   0.1,
+    // )
+    // axe.current.rotation.copy(camera.rotation)
+    // axe.current.position.copy(camera.position).add(camera.getWorldDirection(rotation).multiplyScalar(1))
     api.velocity.set(direction.x, velocity.current[1], direction.z);
     if (jump && Math.abs(velocity.current[1].toFixed(2)) < 0.05)
       api.velocity.set(velocity.current[0], 10, velocity.current[2]);
   });
+
   return <mesh ref={ref} />;
 };
